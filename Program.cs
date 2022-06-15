00e240c57b1aa7ea4d18e133ce3888a065fa52b2
Daixewe <fuentesvd56@gmail.com>
2022-06-15 13:33:34 -0600

    Area if a circle done un c#

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.WriteLine("Area of a circle");
+ 
+ Console.Write("Enter Radius: ");
+
+
+Double Radiuos = Convert.ToDouble(Console.ReadLine());
+Double Area = Math.PI * Radiuos * Radiuos;
+
+Console.WriteLine("Area of the circle");
+Console.WriteLine(Area);
+    
